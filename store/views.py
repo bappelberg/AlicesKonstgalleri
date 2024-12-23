58a2fca4cb625823beb95985cf015bb0f18b25c1
bappelberg <benjamin.w.appelberg@gmail.com>
2024-12-23 11:06:46 +0100

    Update navigation label and enhance product listing page
    - Changed navigation menu label from "Hem" to "Galleri".
    - Modified the index page to display categories and products dynamically.
    - Added a sidebar with category links to filter products.
    - Enhanced the product display with cards, including product images, names, and an "Add to Cart" button.
    - Updated the store view to pass product and category data to the template.

--- store/views.py
+++ store/views.py
@@ -63,7 +63,22 @@ def orders(request):
 
 # Store View
 def store(request):
-    return render(request, 'index.html')
+    cart = request.session.get('cart')
+    if not cart:
+        request.session['cart'] = {}
+    products = None
+    categories = Category.get_all_categories()
+    categoryID = request.GET.get('category')
+    if categoryID:
+        products = Product.get_all_products_by_categoryid(categoryID)
+    else:
+        products = Product.get_all_products();
+
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
+
+    return render(request, 'index.html', data)
 
 
 # Signup View
