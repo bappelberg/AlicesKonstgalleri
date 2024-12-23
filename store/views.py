8921c7ec17d336aa61767da55abfb0e95f7ea444
bappelberg <benjamin.w.appelberg@gmail.com>
2024-12-23 09:20:50 +0100

    Implement signup page and validation logic
    - Updated the signup page layout to include a modern form design with a profile picture and user-friendly form styling.
    - Added fields for the user's first name, last name, phone number, email, and password.
    - Included form validation to ensure all fields are filled correctly, including checks for valid phone number and password length.
    - Implemented backend validation to handle errors like missing or invalid inputs, and email already existing.
    - Updated the signup view to process form data, validate it, and handle errors appropriately.

--- store/views.py
+++ store/views.py
@@ -17,7 +17,6 @@ def login(request):
     return render(request, 'login.html')
 
 
-
 # Orders View
 def orders(request):
     return render(request, 'orders.html')
@@ -30,5 +29,65 @@ def store(request):
 
 # Signup View
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        # Get POST-data
+        first_name = request.POST.get('firstname')
+        last_name = request.POST.get('lastname')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        # Validate data
+        value = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'phone': phone,
+            'email': email
+        }
+        error_message = None
+
+        customer = Customer(
+            first_name=first_name,
+            last_name=last_name,
+            phone=phone,
+            email=email,
+            password=password
+        )
+        error_message = validate_customer(customer)
+
+        if not error_message:
+            customer.password = make_password(customer.password)  # Hash password
+            customer.save()  # Save customer in database
+            return redirect('index')  # If no error, redirect customer to index ingen fel, omdirigera till hemsidan
+        else:
+            data = {
+                'error': error_message,
+                'values': value
+            }
+            return render(request, 'signup.html', data)  # Render signup.html view error messages
+
+    return render(request, 'signup.html')  # Render signup.html for GET method
+
+
+def validate_customer(customer):
+    error_message = None
+    if not customer.first_name:
+        error_message = "Vänligen ange förnamn"
+    elif len(customer.first_name) < 3:
+        error_message = 'Förnamn måste bestå av minst tre bokstäver'
+    elif not customer.last_name:
+        error_message = 'Vänligen ange efternamn'
+    elif len(customer.last_name) < 3:
+        error_message = 'Efternamn måste bestå av minst tre bokstäver'
+    elif not customer.phone:
+        error_message = 'Ange telefonnummer'
+    elif len(customer.phone) < 10:
+        error_message = 'Telefonnummer måste bestå av tio siffror'
+    elif len(customer.password) < 5:
+        error_message = 'Lösenord måste bestå av minst fem tecken'
+    elif len(customer.email) < 5:
+        error_message = 'E-postadresser måste bestå av minst fem tecken'
+    elif customer.is_exists():
+        error_message = 'E-postadressen finns redan registrerad i vårt system'
+    return error_message
 
